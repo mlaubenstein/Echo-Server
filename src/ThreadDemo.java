fa64ae9947c835a248fb217d7576dc28b93494cd
Marvin Laubenstein <37064890+mlaubenstein@users.noreply.github.com>
2018-04-17 14:13:46 +0200

    First try to code an echo server Current Status : -Server and Client can be executed -Server can print "Hello World !" received by Client -Server sends the message from the Client back to the Client -Scanner added -Thread added -NEW: ExecutorService added for a better handling

--- src/ThreadDemo.java
+++ src/ThreadDemo.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.*;
+
 public class ThreadDemo {
 
 
@@ -5,8 +7,32 @@ public class ThreadDemo {
 
     public static void  main (String [] args){
 
-        new Thread (new Count ()).start();
-        
+        System.out.println ( " Countdown: " );
+        ExecutorService executorService = Executors.newFixedThreadPool ( 2 );
+
+        Thread thread1 = new Thread ( new Count () );
+        Thread thread2 = new Thread ( new Count () );
+
+       // thread1.start ();
+       // thread2.start ();
+
+        executorService.execute ( thread1 );
+        executorService.execute ( thread2 );
+        executorService.shutdown ();
+
+        try {
+            //Abwarten, bis der Thread fertig abgelaufen ist
+           // thread1.join ();
+           // thread2.join ();
+            executorService.awaitTermination ( 5, TimeUnit.SECONDS);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace ();
+            //TODO Auto-generated catch block
+        }
+
+        System.out.println ( "GO" );
+
 
     }
 
@@ -14,7 +40,20 @@ public class ThreadDemo {
 
         @Override
         public void run() {
-            for ( int i = 5; i >= 0; i-- ) System.out.println ( "Count:"+ i  );
+            for ( int i = 5; i >= 1; i-- ) {
+
+                System.out.println ( i );
+
+
+                try {
+                    Thread.sleep ( 1000 );
+                } catch (InterruptedException e) {
+                    e.printStackTrace ();
+                    //TODO Auto-generated catch block
+                    }
+            }
+
         }
+
     }
 }
